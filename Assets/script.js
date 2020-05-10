1d237a6dd869043d31351f72de30df8bcce46d49
Juan Osorio <juan.michael.osorio@gmail.com>
2020-05-10 12:03:37 -0500

    Updated containers in html and retreived, and current weather with icon img

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+
+  $.ajax({
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=chicago,il,us&appid=2e066e75d160bdff4f8fab272ec499de',
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var tempFahrenheit = Math.max((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
+    console.log(tempFahrenheit);
+    console.log(response.weather[0].icon);
+
+    $(".today").append(`
+    <div class="container-fluid">
+      <h2 class="row col-md-12 text">${response.name}
+        <img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" alt="${response.weather[0].main} icon" class="icon"/>
+      </h2>
+      <div class="row my-2 details"> Temperature: ${tempFahrenheit}\&degF </div>
+      <div class="row my-2 details"> Wind: ${response.wind.speed}mph,  ${response.wind.deg}\&deg</div>
+      <div class="row my-2 details"> Humidity: ${response.main.humidity}% </div>
+    </div>
+        `);    
+
+  });
+
+
+});
