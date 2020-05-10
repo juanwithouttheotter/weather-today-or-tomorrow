03b0c702b0ae80c785d3b02fbbec82a258f9ad4c
Juan Osorio <juan.michael.osorio@gmail.com>
2020-05-10 14:17:01 -0500

    added date to today card

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-
+  var time = moment();
+  var calendarDate = moment().format('l');
+  console.log(time);
+  console.log(calendarDate);
 
   $.ajax({
     url: 'https://api.openweathermap.org/data/2.5/weather?q=chicago,il,us&appid=2e066e75d160bdff4f8fab272ec499de',
@@ -7,21 +10,20 @@ $(document).ready(function () {
   }).then(function (response) {
     console.log(response);
     var tempFahrenheit = Math.max((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
-    console.log(tempFahrenheit);
-    console.log(response.weather[0].icon);
-
+  
     $(".today").append(`
-    <div class="container-fluid">
-      <h2 class="row col-md-12 text">${response.name}
+    <div class="container">
+      <h2 class="row col-md-12 text">${response.name} \(${calendarDate}\)
         <img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" alt="${response.weather[0].main} icon" class="icon"/>
       </h2>
       <div class="row my-2 details"> Temperature: ${tempFahrenheit}\&degF </div>
-      <div class="row my-2 details"> Wind: ${response.wind.speed}mph,  ${response.wind.deg}\&deg</div>
+      <div class="row my-2 details"> Wind: ${response.wind.speed} mph,  ${response.wind.deg}\&deg</div>
       <div class="row my-2 details"> Humidity: ${response.main.humidity}% </div>
     </div>
         `);    
-
   });
+
+
 
 
 });
